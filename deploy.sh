f2b3d32019a9dc4f1c9fb357e4d27f1060811c0c
Денис <Den160588>
2024-01-05 21:16:46 +0200

    test autodeploy fix 123

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
 
-# Змініть налаштування за необхідності
-REMOTE_DIR=/home/ka522929/sisidev.com.ua/www/
-SSH_USER=ka522929
-
-# Змініть назву секрету, який ви створили
-SERVER_ALIAS=$DEPLOY_SERVER
-
-# Змініть налаштування за необхідності
-git pull origin master
-rsync -avz --exclude='.git' --exclude='wp-config.php' ./ $SSH_USER@$SERVER_ALIAS:$REMOTE_DIR
+cd /home/ka522929/sisidev.com.ua/www
+git pull
