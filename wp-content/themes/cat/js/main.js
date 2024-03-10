78a31bee7173ac04074607371a9596e673c9c084
examp1 <bebe4kaxd@gmail.com>
2024-03-10 14:56:10 +0200

    themes, modes, adaptive table

---
+++ wp-content/themes/cat/js/main.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+	'use strict';
+	document.body.setAttribute('data-theme', 'green-violet')
+	document.body.setAttribute('data-mode','light')
+	document.body.setAttribute('data-windowSize','1440-1280')
+	console.log(app_theme);
+})
