a79894f4ea04e2cde0666821c3c2f92401604905
examp1 <bebe4kaxd@gmail.com>
2024-04-06 22:06:35 +0300

    newvariables and first screen

--- wp-content/themes/cat/views/constructor/service/first-screen.php
+++ wp-content/themes/cat/views/constructor/service/first-screen.php
@@ -73,3 +73,29 @@
 		</div>
 	</div>
 </div>
+<div class="first-screen on-dark bg-lighter section50">
+	<div class="container">
+
+		<div class="text-column">
+			<h1>
+				column 50
+				Усі інструменти для зростання бізнесу у сфері салонного бізнесу
+			</h1>
+			<div class="redactor">
+				<p>
+					The eighth requirement. Raise the healthcare system's focus on the epidemic well-being
+				</p>
+			</div>
+			<div class="btns-container">
+				<div class="btn fill">з заливкою <i class="mdi mdi-check-bold"></i></div>
+				<div class="btn stroke">З БОРДЕРОМ <i class="mdi mdi-check-bold"></i></div>
+			</div>
+
+			<?php require app('path.views') . '/constructor/_buttons.php'; ?>
+
+		</div>
+		<div class="image-column">
+			<img src="https://dfstudio-d420.kxcdn.com/wordpress/wp-content/uploads/2019/06/digital_camera_photo-1080x675.jpg" alt="">
+		</div>
+	</div>
+</div>
