8993c1b22685abd667764b8c0e43dbe8b49f2477
examp1 <bebe4kaxd@gmail.com>
2024-05-12 18:55:43 +0300

    footer iter 1

--- wp-content/themes/cat/views/layouts/sections/footer.php
+++ wp-content/themes/cat/views/layouts/sections/footer.php
@@ -1,3 +1,82 @@
 <footer>
-    footer
+    <div class="container">
+        <div class="footer__menu">
+
+            <div class="menu__item">
+                <a href="#" class="menu__title">
+                    Розділи обліку (Заголовок)
+                </a>
+                <div class="menu__list">
+                    <a href="#" class="menu__link">SaaS рішення</a>
+                    <a href="#" class="menu__link">SaaS рішення</a>
+                    <a href="#" class="menu__link">SaaS рішення</a>
+                    <a href="#" class="menu__link">SaaS рішення</a>
+                    <a href="#" class="menu__link">SaaS рішення</a>
+                    <a href="#" class="menu__link">SaaS рішення</a>
+                </div>
+            </div>
+            <div class="menu__item">
+                <a href="#" class="menu__title">
+                    Розділи обліку (Заголовок)
+                </a>
+                <div class="menu__list">
+                    <a href="#" class="menu__link">SaaS рішення</a>
+                    <a href="#" class="menu__link">SaaS рішення</a>
+                    <a href="#" class="menu__link">SaaS рішення</a>
+                    <a href="#" class="menu__link">SaaS рішення</a>
+                    <a href="#" class="menu__link">SaaS рішення</a>
+                    <a href="#" class="menu__link">SaaS рішення</a>
+                </div>
+            </div>
+            <div class="menu__item">
+                <a href="#" class="menu__title">
+                    Розділи обліку (Заголовок)
+                </a>
+                <div class="menu__list">
+                    <a href="#" class="menu__link">SaaS рішення</a>
+                    <a href="#" class="menu__link">SaaS рішення</a>
+                    <a href="#" class="menu__link">SaaS рішення</a>
+                    <a href="#" class="menu__link">SaaS рішення</a>
+                    <a href="#" class="menu__link">SaaS рішення</a>
+                    <a href="#" class="menu__link">SaaS рішення</a>
+                </div>
+            </div>
+            <div class="menu__item">
+                <a href="#" class="menu__title">
+                    Розділи обліку (Заголовок)
+                </a>
+                <div class="menu__list">
+                    <a href="#" class="menu__link">SaaS рішення</a>
+                    <a href="#" class="menu__link">SaaS рішення</a>
+                    <a href="#" class="menu__link">SaaS рішення</a>
+                    <a href="#" class="menu__link">SaaS рішення</a>
+                    <a href="#" class="menu__link">SaaS рішення</a>
+                    <a href="#" class="menu__link">SaaS рішення</a>
+                </div>
+            </div>
+            <div class="menu__item">
+                <a href="#" class="menu__title">
+                    Розділи обліку (Заголовок)
+                </a>
+                <div class="menu__list">
+                    <a href="#" class="menu__link">SaaS рішення</a>
+                    <a href="#" class="menu__link">SaaS рішення</a>
+                    <a href="#" class="menu__link">SaaS рішення</a>
+                    <a href="#" class="menu__link">SaaS рішення</a>
+                    <a href="#" class="menu__link">SaaS рішення</a>
+                    <a href="#" class="menu__link">SaaS рішення</a>
+                </div>
+            </div>
+
+        </div>
+        <div class="footer__bottom">
+            <ul>
+                <li>Copyright © 2024</li>
+                <li><a href="#">Правила використання</a></li>
+                <li><a href="#">Політика конфіденційності</a></li>
+                <li><a href="#">Управління Cookies</a></li>
+            </ul>
+            <p>Зроблено в Україні</p>
+        </div>
+    </div>
 </footer>
