5c1cd76775d89f75bb1375d5b4e23d9e87d61a40
examp1 <bebe4kaxd@gmail.com>
2024-05-21 12:23:51 +0300

    gallery

--- wp-content/themes/cat/views/constructor/service/block-slider.php
+++ wp-content/themes/cat/views/constructor/service/block-slider.php
@@ -1,35 +1,38 @@
 <?php
-    $data = array_diff_key($content,array_flip(['list']));
+$data = array_diff_key($content, array_flip(['list']));
 ?>
 
-<div class="block-slider">
-    <div class="container">
-        <h3>Карточка слайдер</h3>
-        <pre>
-            <?php
-                print_r($data);
-            ?>
-        </pre>
-        <div class="constrlistwrap">
-            <div class="headerwrap">
-                <?php if (!empty($content['title'])) : ?>
-                    <h2><?php echo $content['title']; ?></h2>
-                <?php endif; ?>
-            </div>
 
-            <?php if (isset($content['list'])) : ?>
-                <ul class="constlist">
-                    <?php foreach ($content['list'] as $item) : ?>
-                        <li>
-                            <h4><?= $item['link']; ?></h4>
-                            <p><?= $item['text']; ?></p>
-                            <img src="<?= get_image_url_by_id($item['image']); ?>" alt="" style="max-width: 300px">
-                        </li>
-                    <?php endforeach; ?>
-                </ul>
+<div class="container">
+    <!-- <pre>
+            <?php
+            print_r($data);
+            ?>
+        </pre> -->
+    <div class="constrlistwrap">
+        <div class="headerwrap">
+            <?php if (!empty($content['title'])) : ?>
+                <h2><?php echo $content['title']; ?></h2>
             <?php endif; ?>
         </div>
     </div>
 </div>
+<div class="swiper1">
 
-<?php require app('path.views') . '/constructor/_buttons.php'; ?>
+    <?php if (isset($content['list'])) : ?>
+        <!-- <ul class="constlist"> -->
+        <?php foreach ($content['list'] as $item) : ?>
+            <div>
+                <h4><?= $item['link']; ?></h4>
+                <p><?= $item['text']; ?></p>
+                <!-- <img src="https://nova-era.sisidev.com.ua/wp-content/uploads/2024/05/img-golovna-test.png" alt=""> -->
+                <img src="<?= get_image_url_by_id($item['image']); ?>" alt="" style="max-width: 300px">
+            </div>
+        <?php endforeach; ?>
+        <!-- </ul> -->
+    <?php endif; ?>
+</div>
+
+<div class="container right">
+    <?php require app('path.views') . '/constructor/_buttons.php'; ?>
+</div>
