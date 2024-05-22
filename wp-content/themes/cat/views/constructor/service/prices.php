191ea9c5d18c5d37f1cb85606ed826b1526e1de2
examp1 <bebe4kaxd@gmail.com>
2024-05-22 16:58:32 +0300

    image border radius and block prices

--- wp-content/themes/cat/views/constructor/service/prices.php
+++ wp-content/themes/cat/views/constructor/service/prices.php
@@ -1,19 +1,40 @@
-<div class="prices">
-    <div class="container">
-        <?php if (!empty($content['title'])) : ?>
-            <h2><?php echo $content['title']; ?></h2>
-        <?php endif; ?>
+<div class="container">
+    <?php if (!empty($content['title'])) : ?>
+        <h2 class="title"><?php echo $content['title']; ?></h2>
+    <?php endif; ?>
 
+    <div class="prices-list">
         <?php if (isset($content['list'])) : ?>
-            <div style="display: flex; gap: 30px">
-                <?php foreach ($content['list'] as $item) : ?>
-                    <pre>
-                        <?php print_r($item);?>
-                    </pre>
-                <?php endforeach; ?>
-            </div>
+            <?php foreach ($content['list'] as $item) : ?>
+                <div class="prices-item">
+                    <div class="main-info">
+                        <?php if ($item['title']) : ?>
+                            <h2>
+                                <?= $item['title']  ?>
+                            </h2>
+                        <?php endif ?>
+                        <?php if ($item['price_title']) : ?>
+                            <p class="price-title <?= $item['price_color'] ?>">
+                                <?= $item['price_title']  ?>
+                            </p>
+                        <?php endif ?>
+                        <?php if ($item['btn__link']) : ?>
+                            <a href="<?= $item['btn__link'] ?>" class="btn <?= $item['btn__color'] . ' ' . $item['btn__size'] . ' ' . $item['btn__type']  ?>">
+                                <?= $item['btn__text'] ?>
+                                <?php if ($item['btn__icon']) : ?>
+                                    <i class="<?= $item['btn__icon'] ?>"></i>
+                                <?php endif ?>
+                            </a>
+                        <?php endif ?>
+                    </div>
+                    <?php if ($item['description']) : ?>
+                        <div class="redactor">
+                            <?= $item['description'] ?>
+                        </div>
+                    <?php endif ?>
+                </div>
+            <?php endforeach; ?>
         <?php endif; ?>
     </div>
+    <?php require app('path.views') . '/constructor/_buttons.php'; ?>
 </div>
-
-<?php require app('path.views') . '/constructor/_buttons.php'; ?>
