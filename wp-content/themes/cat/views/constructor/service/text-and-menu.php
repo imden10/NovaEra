ed7ac2c72b605224b1bc3ec944ea276c8d7acd1f
examp1 <bebe4kaxd@gmail.com>
2024-05-22 13:31:27 +0300

    new block + breadcrumbs

--- wp-content/themes/cat/views/constructor/service/text-and-menu.php
+++ wp-content/themes/cat/views/constructor/service/text-and-menu.php
@@ -1,19 +1,25 @@
-<div class="text-and-menu">
-    <div class="container">
-        <?php if (!empty($content['title'])) : ?>
-            <h1><?php echo $content['title']; ?></h1>
-        <?php endif; ?>
+<div class="container">
+    <div class="inner-wrapper">
+        <div class="aside-menu">
+            <?php if (isset($content['list'])) : ?>
+                <?php foreach ($content['list'] as $item) : ?>
+                    <a href="<?= $item['link_url'] ?>"><?= $item['link_name'] ?></a>
+                <?php endforeach; ?>
+            <?php endif; ?>
+        </div>
 
-        <?php if (!empty($content['text'])) : ?>
-            <?php echo $content['text']; ?>
-        <?php endif; ?>
+        <div class="content">
+            <?php if (!empty($content['title'])) : ?>
+                <h1><?php echo $content['title']; ?></h1>
+            <?php endif; ?>
 
-        <?php if (isset($content['list'])) : ?>
-            <?php foreach ($content['list'] as $item) : ?>
-                <a href="<?= $item['link_url'] ?>"><?= $item['link_name'] ?></a>
-            <?php endforeach; ?>
-        <?php endif; ?>
+            <?php if (!empty($content['text'])) : ?>
+                <div class="redactor">
+                    <?php echo $content['text']; ?>
+                </div>
+            <?php endif; ?>
+        </div>
+
     </div>
+    <?php require app('path.views') . '/constructor/_buttons.php'; ?>
 </div>
-
-<?php require app('path.views') . '/constructor/_buttons.php'; ?>
--- wp-content/themes/cat/views/page/front.php
+++ wp-content/themes/cat/views/page/front.php
@@ -1,15 +1,6 @@
 <section class="page">
     <div class="page__heading">
         <div class="container">
-            <!-- Breadcrumbs -->
-            <!-- <ul class="brdcrmb">
-                <?php $home_page = get_post(get_option('page_on_front')); ?>
-                <li class="brdcrmb__li"><a href="<?php echo site_url(); ?>" class="brdcrmb__lnk"><?php echo $home_page->post_title; ?></a></li>
-
-                <li class="brdcrmb__li"><?php echo !empty($page->page_information_breadcrumb) ? $page->page_information_breadcrumb : $page->post_title; ?></li>
-            </ul> -->
-            <!-- End Breadcrumbs -->
-
             <!-- <h1 class="page__title variablefz"><?php echo $page->post_title; ?></h1> -->
 
             <div class="row justify-content-between">
