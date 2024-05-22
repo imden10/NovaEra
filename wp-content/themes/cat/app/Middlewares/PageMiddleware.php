1953fd36ad05e6e47d88874d3e169d8b8115e407
Денис <Den160588>
2024-05-22 09:53:05 +0300

    PostsMiddleware log

--- wp-content/themes/cat/app/Middlewares/PageMiddleware.php
+++ wp-content/themes/cat/app/Middlewares/PageMiddleware.php
@@ -14,20 +14,20 @@ class PageMiddleware implements StageInterface
 
         $queriedObject = $wp_query->get_queried_object();
 
-        if($_SERVER['REQUEST_URI'] == "/api/form/send"){
-            $formController = $app->make(FormController::class);
-            $formController->sendForm();
-        }
-
-        $path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-        $query = parse_url($_SERVER['REQUEST_URI'], PHP_URL_QUERY);
-        if ($path == "/api/form/render" && strpos($query, 'id=') !== false) {
-            parse_str($query, $params);
-            $id = $params['id'];
-
-            $formController = $app->make(FormController::class);
-            $formController->renderFormView($id);
-        }
+//        if($_SERVER['REQUEST_URI'] == "/api/form/send"){
+//            $formController = $app->make(FormController::class);
+//            $formController->sendForm();
+//        }
+//
+//        $path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+//        $query = parse_url($_SERVER['REQUEST_URI'], PHP_URL_QUERY);
+//        if ($path == "/api/form/render" && strpos($query, 'id=') !== false) {
+//            parse_str($query, $params);
+//            $id = $params['id'];
+//
+//            $formController = $app->make(FormController::class);
+//            $formController->renderFormView($id);
+//        }
 
         $pageController = $app->make(PageController::class);
 
