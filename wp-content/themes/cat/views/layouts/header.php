fa6c9eb07ae7829f0172bcd1b719f6002abecd14
Денис <Den160588>
2024-05-28 13:49:54 +0300

    add meta robots noindex to pages cookies, rules-for-using-the-site, privacy-policy

--- wp-content/themes/cat/views/layouts/header.php
+++ wp-content/themes/cat/views/layouts/header.php
@@ -10,6 +10,10 @@
     <?= config('theme')['fonts'][get_option('theme_settings__font_style')]['link'] ?>
     <link rel="icon" href="<?= get_image_url_by_id(get_option('theme_settings__favicon')) ?>" type="image/png">
     <link rel="stylesheet" href="https://i.icomoon.io/public/0b30d968be/NovaEraNew/style.css">
+    <?php if(in_array($_SERVER['REQUEST_URI'],['/cookies','/rules-for-using-the-site','/privacy-policy'])):?>
+    <meta name="robots" content="noindex">
+    <?php endif;?>
+
 </head>
 
 <?php
