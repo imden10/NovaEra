6297555a83460fe5994eb703bc603d879ec57a9d
Денис <Den160588>
2024-06-03 21:18:53 +0300

    add mini text to text component

--- wp-content/themes/cat/app/Components/MetaBox/Constructor/components/Text.php
+++ wp-content/themes/cat/app/Components/MetaBox/Constructor/components/Text.php
@@ -12,17 +12,32 @@ class Text
             'name' => $name . '[' . $key . '][content][text]',
             'value' => isset($value['content']['text']) ? $value['content']['text'] : ''
         ];
+
+        $miniText = [
+            'name' => $name . '[' . $key . '][content][mini_text]',
+            'value' => isset($value['content']['mini_text']) ? $value['content']['mini_text'] : ''
+        ];
         ?>
 
         <div class="body-block">
             <div class="textarea-part">
+                <label>Текст</label>
                 <textarea id="componentText<?php echo $key; ?>" class="ck-editor" name="<?php echo $text['name']; ?>"><?php echo $text['value']; ?></textarea>
+
+                <br>
+
+                <label>Міні текст</label>
+                <textarea id="componentMiniText<?php echo $key; ?>" class="ck-editor" name="<?php echo $miniText['name']; ?>"><?php echo $miniText['value']; ?></textarea>
             </div>
         </div>
 
         <script type="text/javascript">
             $(document).ready(function() {
                 $('#componentText<?php echo $key; ?>').summernote(summernote_options);
+            });
+
+            $(document).ready(function() {
+                $('#componentMiniText<?php echo $key; ?>').summernote(summernote_options);
             });
         </script>
         <?php
