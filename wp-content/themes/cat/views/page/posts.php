d3a63d078c11563ab1f9d9c5750e89f1769dd8c9
Денис <Den160588>
2024-06-04 19:35:56 +0300

    add breadcrumb

--- wp-content/themes/cat/views/page/posts.php
+++ wp-content/themes/cat/views/page/posts.php
@@ -2,9 +2,8 @@
     <div class="top-side">
         <ul class="breadcrumbs">
             <?php $home_page = get_post(get_option('page_on_front')); ?>
-            <li class="breadcrumbs-item"><a href="<?php echo site_url(); ?>" class="brdcrmb__lnk"><?php echo $home_page->post_title; ?></a></li>
-
-            <li class="breadcrumbs-item"><?php echo !empty($page->page_information_breadcrumb) ? $page->page_information_breadcrumb : $page->post_title; ?></li>
+            <li class="breadcrumbs-item"><a href="<?= $page->page_information_breadcrumb_url ?? site_url() ?>" class="brdcrmb__lnk"><?= $page->page_information_breadcrumb_text ?? $home_page->post_title ?></a></li>
+            <li class="breadcrumbs-item"><?= $page->post_title; ?></li>
         </ul>
         <h1><?php echo $page->post_title; ?></h1>
     </div>
