bd467133debd3f33c8a82911e2f2d8cb4457a540
examp1 <bebe4kaxd@gmail.com>
2024-06-18 23:06:04 +0300

    container

--- wp-content/themes/cat/views/post/single.php
+++ wp-content/themes/cat/views/post/single.php
@@ -2,11 +2,11 @@
     <div class="container top-side">
         <ul class="breadcrumbs">
             <?php $home_page = get_post(get_option('page_on_front')); ?>
-            <?php if ( $post->post_information_breadcrumb_text ): ?>
+            <?php if ($post->post_information_breadcrumb_text) : ?>
                 <li class="breadcrumbs-item"><a href="<?= $post->post_information_breadcrumb_url ?>" class="brdcrmb__lnk"><?= $post->post_information_breadcrumb_text ?></a></li>
-            <?php elseif ($home_page->page_information_breadcrumb_text): ?>
+            <?php elseif ($home_page->page_information_breadcrumb_text) : ?>
                 <li class="breadcrumbs-item"><a href="<?= site_url() ?>" class="brdcrmb__lnk"><?= $home_page->page_information_breadcrumb_text ?></a></li>
-            <?php else: ?>
+            <?php else : ?>
                 <li class="breadcrumbs-item"><a href="<?= site_url() ?>" class="brdcrmb__lnk"><?= $home_page->post_title ?></a></li>
             <?php endif ?>
 
@@ -46,24 +46,27 @@
 <section class="article-container">
 
     <?php buildContentFromConstructorArray('service', $post->post_information_body); ?>
-    <div onclick="history.back()" class="btn primary fill lg back"><i class="ic-chevron-left"></i> Назад</div>
-    <div class="social-share bordered">
-        <span class="date">
-            <?php echo date('d.m.Y', strtotime($post->post_date)); ?>
-        </span>
-        <ul class="share">
-            <li>
-                <a href="https://www.facebook.com/sharer/sharer.php?u=https://nova-era.com.ua" target="_blank" class="ic-facebook" aria-label="Share on Facebook"></a>
-            </li>
-            <li>
-                <a href="https://twitter.com/intent/tweet?url=https://nova-era.com.ua&text=Check%20this%20out!" target="_blank" class="ic-twitter-simple" aria-label="Share on Twitter"></a>
-            </li>
-            <li>
-                <a href="https://www.linkedin.com/shareArticle?mini=true&url=https://nova-era.com.ua" target="_blank" class="ic-linkedin-simple" aria-label="Share on LinkedIn"></a>
-            </li>
-            <li onclick="copyLink()" class="ic-link" aria-label="Copy link">
-            </li>
-        </ul>
+    <div class="container">
+        <div onclick="history.back()" class="btn primary fill lg back"><i class="ic-chevron-left"></i> Назад</div>
+
+        <div class="social-share bordered">
+            <span class="date">
+                <?php echo date('d.m.Y', strtotime($post->post_date)); ?>
+            </span>
+            <ul class="share">
+                <li>
+                    <a href="https://www.facebook.com/sharer/sharer.php?u=https://nova-era.com.ua" target="_blank" class="ic-facebook" aria-label="Share on Facebook"></a>
+                </li>
+                <li>
+                    <a href="https://twitter.com/intent/tweet?url=https://nova-era.com.ua&text=Check%20this%20out!" target="_blank" class="ic-twitter-simple" aria-label="Share on Twitter"></a>
+                </li>
+                <li>
+                    <a href="https://www.linkedin.com/shareArticle?mini=true&url=https://nova-era.com.ua" target="_blank" class="ic-linkedin-simple" aria-label="Share on LinkedIn"></a>
+                </li>
+                <li onclick="copyLink()" class="ic-link" aria-label="Copy link">
+                </li>
+            </ul>
+        </div>
     </div>
 </section>
 
